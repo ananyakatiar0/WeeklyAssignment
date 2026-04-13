bd64da62359bc1fa8a8d5085e461d4fb19255829
Ananya  Katiar <ananyakatiar0@gmail.com>
2026-04-13 14:00:07 +0530

    added q3

---
+++ Q3.java
@@ -0,0 +1,11 @@
+public class Q3 {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles;
+
+        // Given: 1 km = 1.6 miles
+        miles = kilometers * 1.6;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
